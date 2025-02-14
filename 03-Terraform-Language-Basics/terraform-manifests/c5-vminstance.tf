03314d925db9483e5b5ac09e7eaa773b33ade724
Mujeer <mujeer472@gmail.com>
2025-02-14 08:07:38 +0530

    updated the 03 folder

--- 03-Terraform-Language-Basics/terraform-manifests/c5-vminstance.tf
+++ 03-Terraform-Language-Basics/terraform-manifests/c5-vminstance.tf
@@ -6,11 +6,22 @@ resource "google_compute_instance" "myapp1" {
   tags        = [tolist(google_compute_firewall.fw_ssh.target_tags)[0], tolist(google_compute_firewall.fw_http.target_tags)[0]]
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12"
+      image = "debian-cloud/debian-11"
     }
   }
   # Install Webserver
-  metadata_startup_script = file("${path.module}/app1-webserver-install.sh")
+  metadata_startup_script = <<-EOF
+  #!/bin/bash
+  sudo apt install -y telnet
+  sudo apt install -y nginx
+  sudo systemctl enable nginx
+  sudo systemctl start nginx 
+  sudo chmod -R 755 /var/www/html
+  sudo mkdir -p /var/www/html/app1
+  HOSTNAME=$(hostname)
+  sudo echo "<!DOCTYPE html> <html> <body style='background-color:rgb(250, 210, 210);'> <h1>Welcome to StackSimplify - WebVM App1 </h1> <p><strong>VM Hostname:</strong> $HOSTNAME</p> <p><strong>VM IP Address:</strong> $(hostname -I)</p> <p><strong>Application Version:</strong> V1</p> <p>Google Cloud Platform - Demos</p> </body></html>" | sudo tee /var/www/html/app1/index.html
+  sudo echo "<!DOCTYPE html> <html> <body style='background-color:rgb(250, 210, 210);'> <h1>Welcome to StackSimplify - WebVM App1 </h1> <p><strong>VM Hostname:</strong> $HOSTNAME</p> <p><strong>VM IP Address:</strong> $(hostname -I)</p> <p><strong>Application Version:</strong> V1</p> <p>Google Cloud Platform - Demos</p> </body></html>" | sudo tee /var/www/html/index.html
+  EOF
   network_interface {
     subnetwork = google_compute_subnetwork.mysubnet.id 
     access_config {
--- vpc.tf
+++ vpc.tf
@@ -1,41 +1,41 @@
 # Terraform Provider Configuration: google
 provider "google" {
   project = "imposing-muse-421813"
-  region = var.region
+  region  = var.region
 }
 
 # Resource: VPC
 resource "google_compute_network" "myvpc" {
-  name =  var.vpc_name
-  auto_create_subnetworks = false   
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
 }
 
 # Resource: Subnet
 resource "google_compute_subnetwork" "mysubnet" {
-  name          =  var.subnet_name
-  region        =  var.region
-  ip_cidr_range =  var.cidr
-  network       = google_compute_network.myvpc.id  // GET VPC ID
+  name          = var.subnet_name
+  region        = var.region
+  ip_cidr_range = var.cidr
+  network       = google_compute_network.myvpc.id // GET VPC ID
 }
 
 variable "vpc_name" {
-  type = string
+  type    = string
   default = "vpc1"
-  
+
 }
 variable "subnet_name" {
-  type = string
+  type    = string
   default = "subnet1"
-  
+
 }
 variable "region" {
-  type = string
+  type    = string
   default = "us-central1"
-  
-  
+
+
 }
 variable "cidr" {
-  type = string
+  type    = string
   default = "10.128.0.0/20"
 }
 
